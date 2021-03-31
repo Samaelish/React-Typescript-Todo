6f84032ff00ba91ddafcc7269e403f3211677fe0
Samaelish <semnd007@gmail.com>
2021-03-31 21:26:37 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
+import User, { UserInt } from './components/User';
 import './App.css';
 
-function App() {
+const App: React.FC = ()  => {
+
+  interface AllUsersInt {
+    currentUser: UserInt;
+    allUsers: Array<UserInt>
+  }
+
+  const [usersState, setUsersState] = useState<AllUsersInt>({
+    currentUser: { 
+      name: '',
+      age: '',
+      job: '',
+      deleteUser: () => {}
+    },
+    allUsers: []
+  })
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) : void => {
+    setUsersState({
+      ...usersState,
+      currentUser: {
+        ...usersState.currentUser,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  const onSubmitHandler = (e: React.SyntheticEvent) : void => {
+    e.preventDefault();
+    
+    setUsersState({
+      currentUser: {
+        name: '',
+        age: '',
+        job: '',
+        deleteUser: () => {}
+      },
+      allUsers: [
+        ...usersState.allUsers,
+        usersState.currentUser
+      ]
+    })
+
+  }
+
+  const deleteUserHandler = (index: number) : void => {
+    const filterUsers = usersState.allUsers.filter((user, i) => (
+      index !== i
+    ))
+
+    setUsersState({
+      ...usersState,
+      allUsers: filterUsers
+    })
+  }
+
+  const allUsers = usersState.allUsers.map((user, i) => (
+    <User 
+    key={i} 
+    name={user.name} 
+    age={user.age} 
+    job={user.job} 
+    deleteUser={ () => deleteUserHandler(i) }
+    />
+  ))
+
+  console.log(usersState);
+  
 
   return (
     <div className='container'>
-      <h1>React with TypeScript</h1>
-      <form>
+      <h1>React и TypeScript</h1>
+      <h2>Список пользователей</h2>
+      <form onSubmit={onSubmitHandler} className='card'>
         <label htmlFor="userName">Имя: </label>
         <input 
           type="text" 
+          required
           id="userName" 
           name="name" 
-          value="" 
+          value={usersState.currentUser.name} 
+          onChange={onChangeHandler}
           />
         <label htmlFor="userAge">Возраст: </label>
         <input 
           type="number" 
           id="userAge" 
           name="age" 
-          value="" />
+          value={usersState.currentUser.age} 
+          onChange={onChangeHandler}
+          />
         <label htmlFor="userJob">Профессия: </label>
         <input 
           type="text" 
+          required
           id="userJob" 
           name="job" 
-          value="" />
+          value={usersState.currentUser.job} 
+          onChange={onChangeHandler}
+          />
 
-        <button type="submit">Add User</button>
+        <button type="submit" className='submitBtn'>Добавить
+        </button>
       </form>
+      { allUsers }
     </div>
   );
 }
---
+++ src/components/User.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './User.css'
+
+export interface UserInt {
+    name: string;
+    age?: string;
+    job: string;
+    deleteUser: () => void
+}
+
+const User: React.FC<UserInt> = ({ name, age, job, deleteUser }) => {
+    return (
+        <div className='card'>
+            <div className='row'>
+                <h2>Имя: </h2>
+                <p>{name}</p>
+            </div>
+        <hr/>
+        {age && ( 
+        <>
+            <div className='row'>
+                    <h2>Возраст: </h2>
+                    <p>{age}</p>
+                </div> 
+        <hr/> 
+        </>
+        )}
+            <div className='row'>
+                    <h2>Профессия: </h2>
+                    <p>{job}</p>
+                </div>
+        <button className="deleteBtn" onClick={deleteUser}>Удалить</button>
+
+        </div>
+    )
+}
+
+export default User
